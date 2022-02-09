2b295df1a66ed57fb7667f7248387c862e21fa74
Braisbethall <braisbethall@gmail.com>
2022-02-09 08:42:56 +0300

    Update homework2.py

--- homework2.py
+++ homework2.py
@@ -6,32 +6,19 @@ grid = [["-" for i in range(10)] for j in range(10)]
 grid_elements = []
 for cell in grid:
     grid_elements.extend(cell)
-digits = list("1234567890")
+digits = list("0123456789")
 total_moves = 100
 
 
 def print_grid():
-    print(f"""  0 1 2 3 4 5 6 7 8 9
-0 {grid[0][0]} {grid[0][1]} {grid[0][2]} {grid[0][3]} {grid[0][4]} {grid[0][5]} {grid[0][6]} {grid[0][7]}\
- {grid[0][8]} {grid[0][9]}
-1 {grid[1][0]} {grid[1][1]} {grid[1][2]} {grid[1][3]} {grid[1][4]} {grid[1][5]} {grid[1][6]} {grid[1][7]}\
- {grid[1][8]} {grid[1][9]}
-2 {grid[2][0]} {grid[2][1]} {grid[2][2]} {grid[2][3]} {grid[2][4]} {grid[2][5]} {grid[2][6]} {grid[2][7]}\
- {grid[2][8]} {grid[2][9]}
-3 {grid[3][0]} {grid[3][1]} {grid[3][2]} {grid[3][3]} {grid[3][4]} {grid[3][5]} {grid[3][6]} {grid[3][7]}\
- {grid[3][8]} {grid[3][9]}
-4 {grid[4][0]} {grid[4][1]} {grid[4][2]} {grid[4][3]} {grid[4][4]} {grid[4][5]} {grid[4][6]} {grid[4][7]}\
- {grid[4][8]} {grid[4][9]}
-5 {grid[5][0]} {grid[5][1]} {grid[5][2]} {grid[5][3]} {grid[5][4]} {grid[5][5]} {grid[5][6]} {grid[5][7]}\
- {grid[5][8]} {grid[5][9]}
-6 {grid[6][0]} {grid[6][1]} {grid[6][2]} {grid[6][3]} {grid[6][4]} {grid[6][5]} {grid[6][6]} {grid[6][7]}\
- {grid[6][8]} {grid[6][9]}
-7 {grid[7][0]} {grid[7][1]} {grid[7][2]} {grid[7][3]} {grid[7][4]} {grid[7][5]} {grid[7][6]} {grid[7][7]}\
- {grid[7][8]} {grid[7][9]}
-8 {grid[8][0]} {grid[8][1]} {grid[8][2]} {grid[8][3]} {grid[8][4]} {grid[8][5]} {grid[8][6]} {grid[8][7]}\
- {grid[8][8]} {grid[8][9]}
-9 {grid[9][0]} {grid[9][1]} {grid[9][2]} {grid[9][3]} {grid[9][4]} {grid[9][5]} {grid[9][6]} {grid[9][7]}\
- {grid[9][8]} {grid[9][9]}""")
+    print(" ", *digits)
+    row_index = 0
+    for row in grid:
+        print(row_index, end=" ")
+        for element in row:
+            print(element, end=" ")
+        print()
+        row_index += 1
 
 
 def generate_losing_sets():
@@ -87,7 +74,6 @@ while total_moves > 0:
     total_moves -= 1
     which_move += 1
     print_grid()
-    print(generate_losing_sets())
     if win_check():
         print(win_check())
         break
